da296fccb6c0b1a3f23bc5084620ed314e9ff87a
aananya <astream.h@outlook.com>
2018-07-28 16:54:33 +0530

    theory 1

---
+++ HW.java
@@ -0,0 +1,7 @@
+// lets keep our first program as the predictable bsic: Hello world
+
+class FirstOne{  
+    public static void main(String args[]){  
+     System.out.println("Hello World!");  
+    }  
+} 
